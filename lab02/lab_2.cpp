ceeea4184721fb7ca1556b9b2ae6ec6350ecb1ab
Colin Jamison <cjamison@valmagus.com>
2024-09-11 11:49:40 -0700

    Solve problem 4, run formatter

--- lab02/lab_2.cpp
+++ lab02/lab_2.cpp
@@ -2,23 +2,23 @@
 // Author: *** YOUR NAME HERE ***
 
 #include <iostream>
-#include <vector>
 #include <limits.h>
-using namespace std;
+#include <vector>
 
+using namespace std;
 
 /*************************************************************
  * Your solutions go below here and do not change the headers*
  * ***********************************************************/
 
-int triangleNum(int n){
+int triangleNum(int n) {
   if (n == 1) {
     return 1;
   }
   return n + triangleNum(n - 1);
 }
 
-int rFib(int n){
+int rFib(int n) {
   if (n == 0) {
     return 0;
   } else if (n == 1) {
@@ -28,7 +28,7 @@ int rFib(int n){
   return rFib(n - 1) + rFib(n - 2);
 }
 
-int rSumHelper(const vector<int> &v, int i){
+int rSumHelper(const vector<int> &v, int i) {
   if (i == 0) {
     return v[i];
   } else {
@@ -36,47 +36,53 @@ int rSumHelper(const vector<int> &v, int i){
   }
 }
 
-int rSum(const vector<int> &v){
+int rSum(const vector<int> &v) {
   if (v.size() == 0) {
     return 0;
   }
-  return rSumHelper(v, v.size()-1);
+  return rSumHelper(v, v.size() - 1);
 }
 
-int rMaxHelper(const vector<int> &v, int start){
+inline int max(int a, int b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+int rMaxHelper(const vector<int> &v, int start) {
+  if (start == 0) {
+    return v[start];
+  } else {
+    return max(v[start], rMaxHelper(v, start - 1));
+  }
+}
+
+int rMax(const vector<int> &v) {
+  if (v.size() == 0) {
+    return INT_MIN;
+  }
+  return rMaxHelper(v, v.size() - 1);
+}
+
+bool isSorted(const vector<int> &v, int start, int end) { return false; }
+
+bool isPalindromeHelper(string s, int start, int end) { return false; }
+
+bool isPalindrome(string s) { return false; }
+
+int rBinarySearch(const vector<int> &v, int low, int high, int target) {
   return 0;
 }
 
-int rMax(const vector<int> &v){
-  return 0;
-}
-
-bool isSorted(const vector<int> &v, int start, int end){
-  return false;
-}
-
-bool isPalindromeHelper(string s, int start, int end){
-  return false;
-}
-
-bool isPalindrome(string s){
-  return false;
-}
-
-int rBinarySearch(const vector<int> &v, int low, int high, int target){
-  return 0;
-}
-
-bool rSubsetSum(const vector<int> &v, int start, int target){
-  return 0;
-}
+bool rSubsetSum(const vector<int> &v, int start, int target) { return 0; }
 
 /***********************************************
  * Main and helper functions for quick testing *
  * *********************************************/
 void readIntVector(vector<int> &v, int n);
 
-int main(){
+int main() {
   int question = -1;
   cout << "Question: ";
   cin >> question;
@@ -84,63 +90,63 @@ int main(){
   int n = 0, start = 0, end = 0;
   vector<int> v;
   string s = "";
-  switch (question){
-    case 1: //triangleNum
-      cout << "Triangle Number n: ";
-      cin >> n;
-      cout << triangleNum(n) << endl;
-      break;
-    case 2: //rFib
-      cout << "Nth Fibonacci Number: ";
-      cin >> n;
-      cout << rFib(n) << endl;
-      break;
-    case 3: //rSum
-      cout << "Recursive Sum Vector Size: ";
-      cin >> n;
-      cout << "Values: ";
-      readIntVector(v, n);
-      cout << rSum(v) << endl;
-      break;
-    case 4: //rMax
-      cout << "Recursive Max Vector Size: ";
-      cin >> n;
-      cout << "Values: ";
-      readIntVector(v, n);
-      cout << rMax(v) << endl;
-      break;
-    case 5: //isSorted
-      cout << "isSorted Vector Size: ";
-      cin >> n;
-      cout << "Start and End: ";
-      cin >> start >> end;
-      cout << "Values: ";
-      readIntVector(v, n);
-      cout << isSorted(v, start, end) << endl;
-      break;
-    case 6: //isPalindrome
-      cout << "String: ";
-      cin >> s;
-      cout << isPalindrome(s) << endl;
-      break;
-    case 7: //rBinarySearch
-      cout << "Binary Search Vector Size: ";
-      cin >> n;
-      cout << "Target: ";
-      cin >> end;
-      cout << "Values: ";
-      readIntVector(v, n);
-      cout << rBinarySearch(v, 0, v.size()-1, end) << endl;
-      break;
-    case 8: //rSubsetSum
-      cout << "Subset Sum Vector Size: ";
-      cin >> n;
-      cout << "Target: ";
-      cin >> end;
-      cout << "Values: ";
-      readIntVector(v, n);
-      cout << rSubsetSum(v, 0, end) << endl;
-      break;
+  switch (question) {
+  case 1: // triangleNum
+    cout << "Triangle Number n: ";
+    cin >> n;
+    cout << triangleNum(n) << endl;
+    break;
+  case 2: // rFib
+    cout << "Nth Fibonacci Number: ";
+    cin >> n;
+    cout << rFib(n) << endl;
+    break;
+  case 3: // rSum
+    cout << "Recursive Sum Vector Size: ";
+    cin >> n;
+    cout << "Values: ";
+    readIntVector(v, n);
+    cout << rSum(v) << endl;
+    break;
+  case 4: // rMax
+    cout << "Recursive Max Vector Size: ";
+    cin >> n;
+    cout << "Values: ";
+    readIntVector(v, n);
+    cout << rMax(v) << endl;
+    break;
+  case 5: // isSorted
+    cout << "isSorted Vector Size: ";
+    cin >> n;
+    cout << "Start and End: ";
+    cin >> start >> end;
+    cout << "Values: ";
+    readIntVector(v, n);
+    cout << isSorted(v, start, end) << endl;
+    break;
+  case 6: // isPalindrome
+    cout << "String: ";
+    cin >> s;
+    cout << isPalindrome(s) << endl;
+    break;
+  case 7: // rBinarySearch
+    cout << "Binary Search Vector Size: ";
+    cin >> n;
+    cout << "Target: ";
+    cin >> end;
+    cout << "Values: ";
+    readIntVector(v, n);
+    cout << rBinarySearch(v, 0, v.size() - 1, end) << endl;
+    break;
+  case 8: // rSubsetSum
+    cout << "Subset Sum Vector Size: ";
+    cin >> n;
+    cout << "Target: ";
+    cin >> end;
+    cout << "Values: ";
+    readIntVector(v, n);
+    cout << rSubsetSum(v, 0, end) << endl;
+    break;
   }
 
   return 0;
@@ -151,9 +157,9 @@ int main(){
  * v - vector<int> & - the integer vector to fill *
  * n - int - the number of integers to read       *
  * ************************************************/
-void readIntVector(vector<int> &v, int n){
+void readIntVector(vector<int> &v, int n) {
   int m = 0;
-  for (int i = 0; i < n; i++){
+  for (int i = 0; i < n; i++) {
     cin >> m;
     v.push_back(m);
   }

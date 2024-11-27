1508f1255e866a1651eb26e4ed25e67f55b80764
Colin Jamison <cjamison@valmagus.com>
2024-11-27 13:20:20 -0800

    Lab 08 init

---
+++ lab08/Graph.cpp
@@ -0,0 +1,54 @@
+// CSCI 311 
+// Graph class cpp
+// Author:
+
+#include <iostream>
+#include "Graph.h"
+#include <queue>
+#include <limits.h>
+
+//do not change the header files
+//implement the following functions and do not introduce new functions
+
+Graph::Graph(){
+}
+
+void Graph::printAdjList(){
+}
+
+bool Graph::isNeighbor(int u, int v){
+  return false;
+}
+
+void Graph::DFS(){
+}
+
+int Graph::DFSVisit(int s, int time){
+  return time;
+}
+
+void Graph::BFS(int s){
+}
+
+std::vector<int> Graph::distancesFrom(int s){
+}
+
+bool Graph::isTwoColorable(){
+  return false;
+}
+
+bool Graph::isConnected(){
+  return false;
+}
+
+bool Graph::hasCycle(){
+  return false;
+}
+
+bool Graph::hasCycleRecur(int s){
+  return false;
+}
+
+bool Graph::isReachable(int u, int v){
+  return false;
+}
